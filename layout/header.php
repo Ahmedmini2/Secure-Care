1d07960dbc17e1bad66f676348e43e35650e9f6a
mohammed jamal <yamni.zero@gmail.com>
2023-11-20 23:02:58 +0200

    asc

--- layout/header.php
+++ layout/header.php
@@ -1,70 +1,72 @@
+<!-- Header Area Starts -->
+<header class="header-area">
+    <div class="header-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9 d-md-flex">
+                    <h6 class="mr-3"><span class="mr-2"><i class="fa fa-mobile"></i></span> call us now! +966 04 450 8488</h6>
+                    <h6 class="mr-3"><span class="mr-2"><i class="fa fa-envelope-o"></i></span> info@securecare.ae</h6>
+                    <a href="#">
+                        <h6><span class="mr-2"><i class="fa fa-map-marker"></i></span> Find our Location</h6><a href="#">
+                </div>
+                <div class="col-lg-3">
+                    <div class="social-links">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fa fa-vimeo"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="header" id="home">
+        <div class="container">
+            <div class="row align-items-center justify-content-between d-flex">
+                <div id="logo">
+                    <a href="index.html"><img src="assets/images/logo/logo3.png" alt="" title="" /></a>
+                </div>
+                <nav id="nav-menu-container">
+                    <ul class="nav-menu">
 
- 
- <!-- Header Area Starts -->
- <header class="header-area">
-     <div class="header-top">
-         <div class="container">
-             <div class="row">
-                 <div class="col-lg-9 d-md-flex">
-                     <h6 class="mr-3"><span class="mr-2"><i class="fa fa-mobile"></i></span> call us now! +966 04 450 8488</h6>
-                     <h6 class="mr-3"><span class="mr-2"><i class="fa fa-envelope-o"></i></span> info@securecare.ae</h6>
-                     <a href="#"><h6><span class="mr-2"><i class="fa fa-map-marker"></i></span> Find our Location</h6><a href="#">
-                 </div>
-                 <div class="col-lg-3">
-                     <div class="social-links">
-                         <ul>
-                             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                             <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                             <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                             <li><a href="#"><i class="fa fa-vimeo"></i></a></li>
-                         </ul>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
-     <div id="header" id="home">
-         <div class="container">
-             <div class="row align-items-center justify-content-between d-flex">
-                 <div id="logo">
-                     <a href="index.html"><img src="assets/images/logo/logo3.png" alt="" title="" /></a>
-                 </div>
-                 <nav id="nav-menu-container">
-                     <ul class="nav-menu">
+                        <li class="menu-active"><a class="lang" key="home" href="index">Home</a></li>
 
-                         <li class="menu-active"><a class="lang" key="home" href="index">Home</a></li>
-
-                         <li class="menu-has-children"><a class="lang" key="services" href="">Services</a>
-                             <ul>
-                                 <li><a href="equipment">Trading Equipment for Individuals with Determination</a></li>
-                                 <li><a href="paper_products">Trading Paper Products</a></li>
-                                 <li><a href="pharmaceutical">Trading Pharmaceutical Non-Drug Products</a></li>
-                                 <li><a href="cosmetics">Trading Cosmetics & Perfumes</a></li>
-                                 <li><a href="surgical">Trading Surgical and Medical Requisites</a></li>
-                             </ul>
-
-
-                         </li>
-
-                         <li><a class="lang" key="about" href="about">About Us</a></li>
+                        <li class="menu-has-children"><a class="lang" key="services" href="">Services</a>
+                            <ul>
+                                <li><a href="equipment">Trading Equipment for Individuals with Determination</a></li>
+                                <li><a href="paper_products">Trading Paper Products</a></li>
+                                <li><a href="pharmaceutical">Trading Pharmaceutical Non-Drug Products</a></li>
+                                <li><a href="cosmetics">Trading Cosmetics & Perfumes</a></li>
+                                <li><a href="surgical">Trading Surgical and Medical Requisites</a></li>
+                            </ul>
 
 
-                         <li><a class="lang" key="contact" href="">Contact Us</a></li>
+                        </li>
 
-                         <li class="menu-has-children"><a class="lang" key="blogs" href="">Blogs</a>
-                             <ul>
-                                 <li><a href="blog-home">blog home</a></li>
-                                 <li><a href="blog-details">blog details</a></li>
-                             </ul>
-                         </li>
-                         <li>
-                         <button id="ar" class="translate2">عربي</button>
-                         </li>
-                     </ul>
-                 </nav><!-- #nav-menu-container -->
-             </div>
-         </div>
-     </div>
- </header>
- <!-- Header Area End -->
+                        <li><a class="lang" key="about" href="about">About Us</a></li>
+
+
+                        <li><a class="lang" key="contact" href="">Contact Us</a></li>
+
+                        <li class="menu-has-children"><a class="lang" key="blogs" href="">Blogs</a>
+                            <ul>
+                                <li><a href="blog-home">blog home</a></li>
+                                <li><a href="blog-details">blog details</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <button id="ar" class="translate2 btnTran"> 
+                                <button>عربي</button>
+                                <button>English</button>
+                            </button>
+                        </li>
+                    </ul>
+                </nav><!-- #nav-menu-container -->
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Header Area End -->
