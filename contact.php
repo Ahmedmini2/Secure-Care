57fc4e74fa39ee4002328c1b518ee26402e90e32
Ahmedmini2 <liltemes@gmail.com>
2023-11-21 01:44:45 +0400

    يصش

--- contact.php
+++ contact.php
@@ -24,76 +24,81 @@
 </section>
 <!-- Banner Area End -->
 
-    <!-- Map Area Starts -->
-    <section class="map-area section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div id="mapBox" class="mapBox" 
-                        data-lat="40.701083" 
-                        data-lon="-74.1522848" 
-                        data-zoom="13" 
-                        data-info="PO Box CT16122 Collins Street West, Victoria 8007, Australia."
-                        data-mlat="40.701083"
-                        data-mlon="-74.1522848">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Map Area End -->
+<section id="contact" class="contact">
+      <div class="container">
 
-
-    <!-- Contact Form Starts -->
-    <section class="contact-form section-padding3">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                    <div class="d-flex">
-                        <div class="into-icon">
-                            <i class="fa fa-home"></i>
-                        </div>
-                        <div class="info-text">
-                            <h3>Dubai, United Arab Emirates</h3>
-                            <p>Al Kalbani Building - Al Khabaisi - Dubai</p>
-                        </div>
-                    </div>
-                    <div class="d-flex">
-                        <div class="into-icon">
-                            <i class="fa fa-phone"></i>
-                        </div>
-                        <div class="info-text">
-                            <h3>00 (440) 9865 562</h3>
-                            <p>Mon to Fri 9am to 6 pm</p>
-                        </div>
-                    </div>
-                    <div class="d-flex">
-                        <div class="into-icon">
-                            <i class="fa fa-envelope-o"></i>
-                        </div>
-                        <div class="info-text">
-                            <h3>info@securecare.ae</h3>
-                            <p>Send us your query anytime!</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <form action="#">
-                        <div class="left">
-                            <input type="text" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" required>
-                            <input type="email" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" required>
-                            <input type="text" placeholder="Enter subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter subject'" required>
-                        </div>
-                        <div class="right">
-                            <textarea name="message" cols="20" rows="7"  placeholder="Enter Message" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" required></textarea>
-                        </div>
-                        <button type="submit" class="template-btn">subscribe now</button>
-                    </form>
-                </div>
-            </div>
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
-    </section>
-    <!-- Contact Form End -->
+
+      </div>
+
+      <div>
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+      <div class="container">
+
+        <div class="row mt-5">
+
+          <div class="col-lg-6">
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="info-box">
+                  <i class="bx bx-map"></i>
+                  <h3>Our Address</h3>
+                  <p>A108 Adam Street, New York, NY 535022</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-envelope"></i>
+                  <h3>Email Us</h3>
+                  <p>info@example.com<br>contact@example.com</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-phone-call"></i>
+                  <h3>Call Us</h3>
+                  <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="">
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required="">
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required="">
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="7" placeholder="Message" required=""></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
 
 
     <?php include('layout/footer.php'); ?>
