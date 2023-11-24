dfa86100983e8e6d2ef85bbbdf828383f02bca9d
Ahmedmini2 <liltemes@gmail.com>
2023-11-24 21:45:48 +0400

    daw

--- contact.php
+++ contact.php
@@ -70,7 +70,7 @@
 
           </div>
 
-          <div class="col-lg-6">
+          <div class="col-lg-6 pb-4">
             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
               <div class="row">
                 <div class="col-md-6 form-group">
