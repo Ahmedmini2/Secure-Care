68915c736b5e9c2459cb5ad45d713339f01084fc
Ahmedmini2 <liltemes@gmail.com>
2023-11-27 22:00:24 +0400

    daw

--- pharmaceutical.php
+++ pharmaceutical.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Pharmaceutical Non-Drugs Products</h1>
+                    <h1>Pharmaceutical Non-Drug Products</h1>
                     <a href="index">Home</a> <span>|</span> <a href="pharmaceutical">Pharmaceutical</a>
                 </div>
             </div>
@@ -28,14 +28,14 @@
     <section class="welcome-area section-padding">
         <div class="container my-5">
             <div class="row">
-                <div class="col-lg-6">
-                    <img class="w-100 shadow" src="https://via.placeholder.com/824x552" />
-                </div>
+            <video autoplay muted loop id="myVideo" style="width: 824px;position: relative;z-index: 100;">
+                        <source src="assets/images/pharma" type="video/mp4" >
+                    </video>
                 <div class="col-lg-6">
                     <div class="p-5 mt-4">
-                        <h1 class="display-4">H1 Heading</h1>
-                        <p class="lead">Crow's nest schooner ho scallywag hail-shot gabion salmagundi. Doubloon careen code of conduct lugsail hulk ye long clothes. </p>
-                        <a href="#" class="btn btn-outline-dark">Read More</a>
+                        <h1 class="display-4">Food and Herbal Supplements</h1>
+                        <p class="lead">Fuel your well-being with our premium Food and Herbal Supplements. Crafted for optimal nutrition, each supplement is a powerhouse of natural goodness. Elevate your health journey with our carefully selected blends, supporting your vitality from the inside out. Discover the nourishing benefits that nature intended. </p>
+                        
                     </div>
                 </div>
             </div>
