4ba60f8f119ecddd4cea5b28a3ec2c2b3665fe77
Ahmedmini2 <liltemes@gmail.com>
2023-11-27 23:46:35 +0400

    daw

--- surgical.php
+++ surgical.php
@@ -27,34 +27,131 @@
     <!-- Welcome Area Starts -->
     <section class="welcome-area section-padding">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-2 align-self-center">
-                    <div class="welcome-img">
-                        <img src="assets/images/welcome.png" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="welcome-text mt-5 mt-lg-0">
-                        <h2>Welcome to our clinic</h2>
-                        <p class="pt-3">Subdue whales void god which living don't midst lesser yielding over lights whose. Cattle greater brought sixth fly den dry good tree isn't seed stars were.</p>
-                        <p>Subdue whales void god which living don't midst lesser yielding over lights whose. Cattle greater brought sixth fly den dry good tree isn't seed stars were the boring.</p>
-                        <a href="#" class="template-btn mt-3">learn more</a>
-                    </div>
-                </div>
-                <div class="col-lg-2 align-self-center">
-                    <div class="welcome-img">
-                        <img src="assets/images/welcome.png" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="welcome-text mt-5 mt-lg-0">
-                        <h2>Welcome to our clinic</h2>
-                        <p class="pt-3">Subdue whales void god which living don't midst lesser yielding over lights whose. Cattle greater brought sixth fly den dry good tree isn't seed stars were.</p>
-                        <p>Subdue whales void god which living don't midst lesser yielding over lights whose. Cattle greater brought sixth fly den dry good tree isn't seed stars were the boring.</p>
-                        <a href="#" class="template-btn mt-3">learn more</a>
-                    </div>
+        <div class="row">
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
                 </div>
             </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Surgical Instruments</h2>
+                    <p>Explore a comprehensive range of precision surgical tools, including scalpels, forceps, scissors, sutures, and retractors. Elevate surgical precision with our top-quality instruments.</p>
+                </div>
+            </div>
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Diagnostic Equipment</h2>
+                    <p>Stay at the forefront of healthcare diagnostics with our array of tools, from blood pressure monitors and stethoscopes to otoscopes and thermometers. Reliable equipment for accurate assessments.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Wound Care Supplies</h2>
+                    <p>Ensure optimal wound healing with our extensive selection of dressings, bandages, wound closure strips, and antiseptic solutions. Quality care for effective recovery.</p>
+                </div>
+            </div>
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Sterilization and Infection Control</h2>
+                    <p>Uphold the highest standards of hygiene with our sterilization solutions, including autoclaves, sterilization wraps, disinfectants, and hand sanitizers. A commitment to infection prevention.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Patient Monitoring Devices</h2>
+                    <p>Monitor patient health seamlessly with our range of devices, from pulse oximeters and ECG machines to blood glucose monitors and holter monitors. Precision in patient care.</p>
+                </div>
+            </div>
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Orthopedic Supplies</h2>
+                    <p>Support orthopedic wellness with our collection, featuring braces, orthopedic implants, casting materials, and prosthetics/orthotics. Comprehensive solutions for musculoskeletal health.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Anesthesia and Respiratory Equipment</h2>
+                    <p>Ensure patient comfort with our anesthesia and respiratory equipment, including machines, ventilators, oxygen masks, and nebulizers. A breath of reliability in medical care.</p>
+                </div>
+            </div>
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Emergency Medical Equipment</h2>
+                    <p>Respond effectively to emergencies with our range of equipment, including defibrillators, first aid kits, emergency stretchers, and ambulance supplies. Preparedness in critical moments.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Hospital Furniture and Accessories</h2>
+                    <p>Create a conducive healthcare environment with our hospital furniture, including examination tables, hospital beds, IV stands, and medical carts. Comfort and efficiency in healthcare spaces.</p>
+                </div>
+            </div>
+            <div class="col-lg-2 align-self-center">
+                <div class="welcome-img">
+                    <img src="assets/images/welcome.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="welcome-text mt-5 mt-lg-0">
+                    <h2>Medical Apparel</h2>
+                    <p>Equip healthcare professionals with quality apparel, including scrubs, lab coats, surgical gowns, and protective eyewear. Style meets functionality in medical wear.</p>
+                </div>
+            </div>
+        </div>
+
         </div>
     </section>
     <!-- Welcome Area End -->
