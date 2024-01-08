e88ef99d61584680b78d73c38ea5459236816f9f
Ahmedmini2 <liltemes@gmail.com>
2024-01-08 13:44:01 +0400

    daw

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,7 +21,7 @@ flkty.on('scroll', function () {
   flkty.slides.forEach(function (slide, i) {
     var image = slides[i];
     var x = (slide.target + flkty.x) * -1/3;
-    image.style.backgroundPosition = x + 'px';
+    image.style.backgroundPosition = x + 'py';
   });
 });
       window.onload = function() {
